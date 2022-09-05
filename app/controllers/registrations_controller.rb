5f127b5f0c8fce59e9699a9d6cc03fa3e7361ec6
Diego Aquilina <diegoaquilina@hotmail.com>
2022-09-05 17:26:33 -0300

    rotas de cadastro login, cadastro de pet, footer, carousel e alguns botoes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,13 @@ class RegistrationsController < Devise::RegistrationsController
   def after_update_path_for(resource)
     profile_path
   end
+
+  def after_sign_in_path_for(resource)
+    pets_path
+  end
+
+  def after_sign_up_path_for(resource)
+    pets_path
+  end
+
 end
