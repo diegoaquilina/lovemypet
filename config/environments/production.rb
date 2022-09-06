f94098609f02d1632d0d819cd2b9607d19652df5
Fabiano Garcia <fabianogarcia00@hotmail.com>
2022-09-06 06:55:10 -0700

    Add dotenv - Protect my secret data in .env file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,10 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+  config.action_mailer.default_url_options = { host: "https://lovemypetapp.herokuapp.com/" }
   # Settings specified here will take precedence over those in config/application.rb.
+
+  config.action_mailer.delivery_method = :smtp
 
   # Code is not reloaded between requests.
   config.cache_classes = true
